4bf5ef66bb6e0eb40281051d2f721275be28beee
artidoro <apagnoni@college. harvard.edu>
2016-03-22 03:53:33 -0400

    strassens works, modified addition because it was assuming the result matrix was all 0, but it isn't

--- main.cpp
+++ main.cpp
@@ -69,38 +69,76 @@ void subtract(Matrix* A, Matrix* B, Matrix* C, int topA, int leftA, int topB, in
 }
 
 void convMult(Matrix* A, Matrix* B, Matrix* C, int topA, int leftA, int topB, int leftB, int topC, int leftC, int dimension) {
-
-	for (int i = 0; i < dimension; ++i)
+    for (int i = 0; i < dimension; ++i)
 		for (int j = 0; j < dimension; ++j)
 			for (int k = 0; k < dimension; ++k)
-				C->matrix[topC + i][leftC + j] += A->matrix[topA + i][leftA + k] * B->matrix[topB + k][leftB + j];
+                if (k == 0) 
+                    C->matrix[topC + i][leftC + j] = A->matrix[topA + i][leftA + k] * B->matrix[topB + k][leftB + j]; 
+				else
+                    C->matrix[topC + i][leftC + j] += A->matrix[topA + i][leftA + k] * B->matrix[topB + k][leftB + j];
 }
 
 void multiply(Matrix*, Matrix*, Matrix*, int , int , int , int , int , int , int , int);
 
 void strassenMult(Matrix* A, Matrix* B, Matrix* C, int topA, int leftA, int topB, int leftB, int topC, int leftC, int dimension) {
     Matrix* T1 = new Matrix();
-    initMatrix(T1, dimension / 2); // TODO deal with non-power of 2 case
+    initMatrix(T1, dimension/2); // TODO deal with non-power of 2 case
     Matrix* T2 = new Matrix();
-    initMatrix(T2, dimension / 2); // TODO deal with non-power of 2 case
+    initMatrix(T2, dimension/2); // TODO deal with non-power of 2 case
     
     // C12 = A21 - A11
-    subtract(A, A, C, dimension / 2, 0, 0, 0, 0, dimension / 2, dimension/2);
-    printMatrix(*C);
+    subtract(A, A, C, dimension/2, 0, 0, 0, 0, dimension/2, dimension/2);
     // C21 = B11 + B12
     add(B,B,C,0,0,0,dimension/2,dimension/2,0,dimension/2);
-    printMatrix(*C);
     // C22 = C12 * C21
     multiply(C,C,C,0,dimension/2,dimension/2,0,dimension/2,dimension/2,dimension/2,2);
-    printMatrix(*C);
     //C12 = A12 - A22
     subtract(A,A,C,0,dimension/2,dimension/2,dimension/2,0,dimension/2,dimension/2);
-    printMatrix(*C);
     //C21 = B21 + B22
     add(B, B, C, dimension/2,0,dimension/2,dimension/2,dimension/2,0,dimension/2);
-    printMatrix(*C);
-    
-    
+    //C11 = C12 * C21
+    multiply(C,C,C,0,dimension/2,dimension/2,0,0,0,dimension/2,2);
+    //C12 = A11 + A22
+    add(A, A, C, 0, 0, dimension/2, dimension/2, 0, dimension/2, dimension/2);
+    //C21 = B11 + B22
+    add(B,B,C,0,0,dimension/2,dimension/2,dimension/2,0,dimension/2);
+    //T1 = C12*C21
+    multiply(C,C,T1,0,dimension/2,dimension/2,0,0,0,dimension/2,2);
+    //C11 = T1 + C11
+    add(T1,C,C,0,0,0,0,0,0,dimension/2);
+    //C22 = T1 + C22
+    add(T1,C,C,0,0,dimension/2,dimension/2,dimension/2,dimension/2,dimension/2);
+    //T2 = A21 + A22
+    add(A,A,T2,dimension/2,0,dimension/2,dimension/2,0,0,dimension/2);
+    //C21 = T2 * B11
+    multiply(T2,B,C,0,0,0,0,dimension/2,0,dimension/2,2);
+    //C22 = C22 - C21
+    subtract(C,C,C,dimension/2,dimension/2,dimension/2,0,dimension/2,dimension/2,dimension/2);
+    //T1 = B21 - B11
+    subtract(B,B,T1,dimension/2,0,0,0,0,0,dimension/2);
+    //T2 = A22 * T1
+    multiply(A,T1,T2,dimension/2,dimension/2,0,0,0,0,dimension/2,2);
+    //C21 = C21 + T2
+    add(C,T2,C,dimension/2,0,0,0,dimension/2,0,dimension/2);
+    //C11 = C11 + T2
+    add(C,T2,C,0,0,0,0,0,0,dimension/2);
+    //T1 = B12 - B22
+    subtract(B,B,T1,0,dimension/2,dimension/2,dimension/2,0,0,dimension/2);
+    //C12 = A11 * T1
+    multiply(A,T1,C,0,0,0,0,0,dimension/2,dimension/2,2);
+    //C22 = C22 + C12
+    add(C,C,C,dimension/2,dimension/2,0,dimension/2,dimension/2,dimension/2,dimension/2);
+    //T2 = A11 + A12
+    add(A,A,T2,0,0,0,dimension/2,0,0,dimension/2);
+    //T1 = T2 * B22
+    multiply(T2,B,T1,0,0,dimension/2,dimension/2,0,0,dimension/2,2);
+    //C12 = C12 + T1
+    add(C,T1,C,0,dimension/2,0,0,0,dimension/2,dimension/2);
+    //C11 = C11 - T1
+    subtract(C,T1,C,0,0,0,0,0,0,dimension/2);
+
+    delete(T1);
+    delete(T2);
 }
 
 void multiply(Matrix* A, Matrix* B, Matrix* C, int topA, int leftA, int topB, int leftB, int topC, int leftC, int dimension, int threshold){
@@ -129,40 +167,13 @@ void testConvMult(){
     Matrix* D = new Matrix();
     initMatrix(D,3);
 
-    A->matrix[0][0] = 1;
-    A->matrix[0][1] = 2;
-    A->matrix[0][2] = -3;
-    A->matrix[1][0] = 1;
-    A->matrix[1][1] = 3;
-    A->matrix[1][2] = 0;
-    A->matrix[2][0] = 2;
-    A->matrix[2][1] = -1;
-    A->matrix[2][2] = 1;
-
-    B->matrix[0][0] = 2;
-    B->matrix[0][1] = 1;
-    B->matrix[0][2] = 0;
-    B->matrix[1][0] = -2;
-    B->matrix[1][1] = -1;
-    B->matrix[1][2] = 1;
-    B->matrix[2][0] = 2;
-    B->matrix[2][1] = 1;
-    B->matrix[2][2] = -3;
-
-    C->matrix[0][0] = -8;
-    C->matrix[0][1] = -4;
-    C->matrix[0][2] = 11;
-    C->matrix[1][0] = -4;
-    C->matrix[1][1] = -2;
-    C->matrix[1][2] = 3;
-    C->matrix[2][0] = 8;
-    C->matrix[2][1] = 4;
-    C->matrix[2][2] = -4;
+    A->matrix = {{1, 2, -3}, {1, 3, 0}, {2, -1, 1}};
+    B->matrix = {{2, 1, 0}, {-2, -1, 1}, {2, 1, -3}};
+    C->matrix = {{-8, -4, 11}, {-4, -2, 3}, {8, 4, -4}};
     
     convMult(A,B,D,0,0,0,0,0,0,3);
  
     assert(isEqual(D, C));
-    cout << "All tests pass!" << endl;
 
 }
 
@@ -178,12 +189,10 @@ void testStrasMult(){
     
     A->matrix = {{1,2},{3,1}};
     B->matrix = {{2,1},{3,1}};
-    
-    C->matrix = {{0,1},{2,6}};
-    printMatrix(*C);
+    C->matrix = {{8,3},{9,4}};
     
     multiply(A, B, D, 0, 0, 0, 0, 0, 0, 2, 2);
-    
+    assert(isEqual(D, C));
     
 
 }
@@ -192,7 +201,7 @@ void testStrasMult(){
 
 int main(){
     testStrasMult();
-	//testConvMult();
+	testConvMult();
     
     return 0;
 }
